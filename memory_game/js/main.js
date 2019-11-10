b3bea4fb20ae1e1f89df313a4484a5c414072496
Malcom Lipscomb Jr <malcomlipscombjr.@Malcoms-MacBook.local>
2019-11-10 14:16:13 -0700

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,8 +1,18 @@
-console.log("Up and running!");
-var cardOne = "queen";
-var cardTwo = "king";
-var cardThree = "queen";
-var cardFour = "king";
 
-console.log("User flipped " + cardFour);
-console.log("User flipped " + cardOne);
+const cards = ["queen", "queen", "king", "king"];
+
+var cardsInPlay = [];
+
+var cardOne = cards[1];
+cardsInPlay.push(cards[0]);
+console.log("User flipped queen");
+
+var cardTwo = cards[2];
+cardsInPlay.push(cards[1]);
+console.log("User flipped queen");
+
+if (cardsInPlay[0] === cardsInPlay[1]) {
+	alert("You found a match!");
+} else {
+	alert("sorry, try again");
+}
